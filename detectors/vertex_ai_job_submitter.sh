c0951c18ba1359a1314cf5d9636d09aa4fecf58b
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2022-03-18 18:24:04 +0530

    Feat: Update job submitter
    * Updated Job submitter script with a commented version of command which uses accelerators while training models.

--- detectors/vertex_ai_job_submitter.sh
+++ detectors/vertex_ai_job_submitter.sh
@@ -5,6 +5,8 @@ export PROJECT='text-analysis-323506'
 export REGION='us-central1'
 export BUCKET='text-analysis-323506'
 export MACHINE_TYPE='n1-highmem-4'
+export ACCELERATOR_TYPE=NVIDIA_TESLA_K80
+export ACCELERATOR_COUNT=1
 export REPLICA_COUNT=1
 export EXECUTE_IMAGE_URI='us-docker.pkg.dev/vertex-ai/training/tf-gpu.2-8:latest'
 export LOCAL_PACKAGE_PATH='/home/jupyter/Sentiment-Analysis-of-Amazon-Reviews'
@@ -24,3 +26,11 @@ gcloud ai custom-jobs create \
     --display-name=$JOBNAME \
     --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,executor-image-uri=$EXECUTE_IMAGE_URI,local-package-path=$LOCAL_PACKAGE_PATH,python-module=$PYTHON_MODULE \
     --args=--train-config=gs://$BUCKET/config/config.yaml
+
+
+# Submit training job with accelerators
+#gcloud ai custom-jobs create \
+#    --region=$REGION \
+#    --display-name=$JOBNAME \
+#    --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,accelerator-type=$ACCELERATOR_TYPE,accelerator-count=$ACCELERATOR_COUNT,executor-image-uri=$EXECUTE_IMAGE_URI,local-package-path=$LOCAL_PACKAGE_PATH,python-module=$PYTHON_MODULE \
+#    --args=--data-dir=gs://text-analysis-323506/data,--save-dir=gs://text-analysis-323506/saved_model,--batch-size=1024
